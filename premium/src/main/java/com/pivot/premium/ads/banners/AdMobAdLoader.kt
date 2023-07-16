9078293a6ed347bae84ea5bbd07bf822565a483b
shaharalgaro <shaharal@zipoapps.com>
2023-07-16 18:06:01 +0200

    upgrade banners with anchored

--- premium/src/main/java/com/pivot/premium/ads/banners/AdMobAdLoader.kt
+++ premium/src/main/java/com/pivot/premium/ads/banners/AdMobAdLoader.kt
@@ -1,6 +1,7 @@
 package com.pivot.premium.ads.banners
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.ViewGroup
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
@@ -23,43 +24,43 @@ class AdMobAdLoader(val context: Context) {
     suspend fun loadAd(adSize: PremiumBannerView.PremiumAdSize): ViewGroup? {
         return suspendCancellableCoroutine { cont ->
             mAdView = AdView(context)
-            mAdView?.setAdSize(if(adSize == PremiumBannerView.PremiumAdSize.BANNER) AdSize.BANNER else AdSize.MEDIUM_RECTANGLE)
+            mAdView?.setAdSize(if(adSize == PremiumBannerView.PremiumAdSize.BANNER) getAnchoredSize() else AdSize.MEDIUM_RECTANGLE)
             mAdView?.adUnitId = context.getString(R.string.banner_ad_unit)
 
             val adRequest = AdRequest.Builder().build()
 
             mAdView?.adListener = object: AdListener() {
                 override fun onAdLoaded() {
-                    // Code to be executed when an ad finishes loading.
                     if(cont.isActive) {
                         cont.resume(mAdView!!)
                     }
                 }
 
                 override fun onAdFailedToLoad(adError : LoadAdError) {
-                    // Code to be executed when an ad request fails.
                     if(cont.isActive) {
                         cont.resume(null)
                     }
                 }
 
                 override fun onAdOpened() {
-                    // Code to be executed when an ad opens an overlay that
-                    // covers the screen.
                 }
 
                 override fun onAdClicked() {
-                    // Code to be executed when the user clicks on an ad.
                 }
 
                 override fun onAdClosed() {
-                    // Code to be executed when the user is about to return
-                    // to the app after tapping on an ad.
                 }
             }
             CoroutineScope(Dispatchers.Main).launch {
                 mAdView?.loadAd(adRequest)
             }
         }
+    }
+
+    private fun getAnchoredSize(): AdSize {
+        val density = Resources.getSystem().displayMetrics.density
+        val adWidth = (Resources.getSystem().displayMetrics.widthPixels / density).toInt()
+
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, adWidth)
     }
 }
